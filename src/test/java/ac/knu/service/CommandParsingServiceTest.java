4cd3c82b69ee787f43035d4788f30107b96b6ce4
Han seungjin <suku10@kangwon.ac.kr>
2018-10-20 21:32:06 +0900

    1. CommandParsingService switch문 메소드구현 2. 친구목록 10명 넘어가면 경고메시지 3. 영어로 입력된 이름 모두 소문자로 변환

--- src/test/java/ac/knu/service/CommandParsingServiceTest.java
+++ src/test/java/ac/knu/service/CommandParsingServiceTest.java
@@ -46,6 +46,13 @@ public class CommandParsingServiceTest {
     }
 
     @Test
+    public void 대소문자만_차이나는_같은_이름의_친구를_추가하려고_하면_경고_메시지_출력(){
+        commandParsingService.parseCommand("add Kim 23 F");
+        String command = commandParsingService.parseCommand("add kim 23 F");
+        assertEquals("Add fail!: Name duplication", command);
+    }
+
+    @Test
     public void bot_should_work_remove_command() {
         String command = commandParsingService.parseCommand("remove 김재성");
         assertEquals("Remove done!", command);
@@ -77,9 +84,9 @@ public class CommandParsingServiceTest {
         String command = commandParsingService.parseCommand("list");
         StringBuilder result = new StringBuilder(String.format("%-20s| %s\t| %s\n", "Name", "Age", "Gender"));
         result.append("------------------------------------\n");
-        result.append(String.format("%-20s| %s\t| %s\n", "Amy", "22", Gender.FEMALE));
-        result.append(String.format("%-20s| %s\t| %s\n", "Sam", "21", Gender.MALE));
-        result.append(String.format("%-20s| %s\t| %s\n", "Sophia", "23", Gender.FEMALE));
+        result.append(String.format("%-20s| %s\t| %s\n", "amy", "22", Gender.FEMALE));
+        result.append(String.format("%-20s| %s\t| %s\n", "sam", "21", Gender.MALE));
+        result.append(String.format("%-20s| %s\t| %s\n", "sophia", "23", Gender.FEMALE));
         result.append(String.format("%-20s| %s\t| %s\n", "김재성", "22", Gender.MALE));
         assertTrue(command.contains(result.toString()));
     }
@@ -106,5 +113,20 @@ public class CommandParsingServiceTest {
     public void bot_should_work_time_command() {
         String command = commandParsingService.parseCommand("time");
         assertTrue(command.contains("Current Time is"));
+    }
+
+    @Test
+    public void 열명_이상의_친구를_추가하면_경고_메시지_출력(){
+        commandParsingService.parseCommand("add Sam 21 M");
+        commandParsingService.parseCommand("add Amy 22 F");
+        commandParsingService.parseCommand("add Sophia 23 F");
+        commandParsingService.parseCommand("add Noah 20 M");
+        commandParsingService.parseCommand("add Liam 21 F");
+        commandParsingService.parseCommand("add John 22 M");
+        commandParsingService.parseCommand("add Wendy 23 F");
+        commandParsingService.parseCommand("add Ryan 20 M");
+        commandParsingService.parseCommand("add Jin 20 M");
+        String command = commandParsingService.parseCommand("add Ruby 21 F");
+        assertEquals("If you want to add a new fiend, Remove friend first",command);
     }
 }
